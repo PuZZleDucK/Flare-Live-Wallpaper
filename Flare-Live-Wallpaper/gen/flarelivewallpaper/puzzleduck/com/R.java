cd9dcdffcd867109e6d7d21c2f152c827d2c1f55
PuZZleDucK <puzzleduck@gmail.com>
2012-05-07 02:56:45 +1000

    Strings and icons

--- Flare-Live-Wallpaper/gen/flarelivewallpaper/puzzleduck/com/R.java
+++ Flare-Live-Wallpaper/gen/flarelivewallpaper/puzzleduck/com/R.java
@@ -11,13 +11,19 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int flare_lwp=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int flare_settings=0x7f050003;
+        public static final int flare_wallpaper_service_label=0x7f050002;
+        public static final int hello=0x7f050000;
+    }
+    public static final class xml {
+        public static final int flare_lwp=0x7f040000;
     }
 }
