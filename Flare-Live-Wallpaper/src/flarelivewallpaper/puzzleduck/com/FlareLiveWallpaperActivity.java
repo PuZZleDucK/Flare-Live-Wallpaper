085ff381d1de30c2aa05a1ae456c6be7ea132c7d
Ben Minerds <puzzleduck@gmail.com>
2012-05-06 19:03:51 +1000

    initial commit, setup git

---
+++ Flare-Live-Wallpaper/src/flarelivewallpaper/puzzleduck/com/FlareLiveWallpaperActivity.java
@@ -0,0 +1,13 @@
+package flarelivewallpaper.puzzleduck.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FlareLiveWallpaperActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
