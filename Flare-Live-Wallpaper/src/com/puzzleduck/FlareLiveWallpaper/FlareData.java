2500b3aa43d7de4ec1b372ace81d3ec12c6a4152
PuZZleDucK <PuZZleDucK@gmail.com>
2012-05-09 15:13:19 +1000

    Moving movement and rendering code into flare data class

--- Flare-Live-Wallpaper/src/com/puzzleduck/FlareLiveWallpaper/FlareData.java
+++ Flare-Live-Wallpaper/src/com/puzzleduck/FlareLiveWallpaper/FlareData.java
@@ -1,4 +1,5 @@
 package com.puzzleduck.FlareLiveWallpaper;
+import android.graphics.*;
 
 public class FlareData {
 	private float x;
@@ -54,6 +55,162 @@ public class FlareData {
 		explosionCount = 0;
 		explosion2Count = 0;
 		explosion2Radius = 0;
+	}
+
+	public void render(Canvas c, Paint mPaint)
+	{
+		// TODO: Implement this method
+
+		//render
+		if (thisFlare.getTime() < thisFlare.getStage1Time()) {
+			//stage 1
+//			            Log.d(TAG, "S1");
+//						mPaint.setColor(0xFF00FF00);
+			mPaint.setColor(0xCCDDDD00);
+//						c.drawCircle(thisFlare.getX(), thisFlare.getY(), 3, mPaint);
+			c.drawRoundRect(new RectF(thisFlare.getX(), thisFlare.getY(), thisFlare.getX()+1, thisFlare.getY()+5), 0, 0, mPaint);
+		} else {
+			if (thisFlare.getTime() < thisFlare.getStage1Time() + thisFlare.getStage2Time()) {
+				//stage 2
+//				            Log.d(TAG, "S2");
+
+				if(thisFlare.getExplosionCount() == 0) //new explosion
+				{
+					thisFlare.setExplosionCount(6 + rng.nextInt(20) );
+//								Log.d(TAG, "\n\nFlare count: " + thisFlare.getExplosionCount());
+
+//								for(int i = thisFlare.getExplosionCount(); i > 0; i--)
+//								{	
+//									Log.d(TAG, "   -flare" + i +": " + thisFlare.getExplosionCount());
+//									Log.d(TAG, "        i/count " + (double)i/(double)thisFlare.getExplosionCount() );
+//									Log.d(TAG, "        i/count*360 " + (double)((double)i/(double)thisFlare.getExplosionCount())*360.0 );
+//									Log.d(TAG, "        rad sin i/count*360 " + (float)((double)thisFlare.getExplosionRadius()+20) * Math.sin((double)((double)i/(double)thisFlare.getExplosionCount())*359.0));
+//									Log.d(TAG, "        rad cos i/count*360 " + (float)((double)thisFlare.getExplosionRadius()+20) * Math.cos((double)((double)i/(double)thisFlare.getExplosionCount())*359.0));
+//								}
+				}
+
+				mPaint.setColor(thisFlare.getColor1());
+				thisFlare.incrementExplosionRadius();
+				thisFlare.incrementExplosionRadius();
+				//fade colors
+				if(rng.nextInt(4) == 1)
+				{
+					thisFlare.setColor1(thisFlare.getColor1()-0x01000000);
+				}
+				//drift down
+				if(rng.nextInt(40) == 1)
+				{
+					thisFlare.setY(thisFlare.getY()+1);
+				}
+
+				for(int i = thisFlare.getExplosionCount(); i > 0; i--)
+				{
+					c.drawCircle(
+						(float)((double)thisFlare.getExplosionRadius() * (Math.sin((double)((double)i/(double)thisFlare.getExplosionCount())*0.017453293*360)) + thisFlare.getX() + rng.nextInt(2)-4),
+						(float)((double)thisFlare.getExplosionRadius() * (Math.cos((double)((double)i/(double)thisFlare.getExplosionCount())*0.017453293*360)) + thisFlare.getY() + rng.nextInt(2)-4),
+						1,
+						mPaint);
+//								c.drawCircle(
+//										(float) (thisFlare.getX() + (Math.cos( (i/thisFlare.getExplosionCount())*360 ) * thisFlare.getExplosionRadius())),
+//										(float) (thisFlare.getY() + (Math.sin((i/thisFlare.getExplosionCount())*360) * thisFlare.getExplosionRadius())),
+//										2,
+//										mPaint);
+				}
+			}else
+			{
+				if(thisFlare.getTime() < thisFlare.getStage1Time() + thisFlare.getStage2Time() + thisFlare.getStage3Time())
+				{
+					//stage 3
+					mPaint.setColor(thisFlare.getColor2());
+					thisFlare.incrementExplosion2Radius();
+
+					if(thisFlare.getExplosion2Count() == 0) //new explosion
+					{
+						thisFlare.setExplosion2Count(4 + rng.nextInt(8) );
+						//								Log.d(TAG, "\n\nFlare2 count: " + thisFlare.getExplosionCount());
+
+					}
+
+
+
+					for(int i = thisFlare.getExplosionCount(); i > 0; i--)
+					{
+						for(int j = thisFlare.getExplosion2Count(); j > 0; j--)
+						{
+
+							switch(rng.nextInt(5))
+							{
+								case(0):
+									mPaint.setColor(thisFlare.getColor2()-0x2200000);
+
+								case(2):
+
+									mPaint.setColor(thisFlare.getColor2()+0x22000000);
+									break;
+								default:
+									mPaint.setColor(thisFlare.getColor2());
+									break;
+							}
+							c.drawPoint(
+								(float)( (double)thisFlare.getExplosion2Radius() * (Math.sin((double)((double)j/(double)thisFlare.getExplosion2Count())*0.017453293*360)) +     (double)thisFlare.getExplosionRadius() * (Math.sin((double)((double)i/(double)thisFlare.getExplosionCount())*0.017453293*360)) + thisFlare.getX() + rng.nextInt(2)-4),
+								(float)( (double)thisFlare.getExplosion2Radius() * (Math.cos((double)((double)j/(double)thisFlare.getExplosion2Count())*0.017453293*360)) +     (double)thisFlare.getExplosionRadius() * (Math.cos((double)((double)i/(double)thisFlare.getExplosionCount())*0.017453293*360)) + thisFlare.getY() + rng.nextInt(2)-4),
+								mPaint);
+
+						}
+
+						//fade colors
+						if(rng.nextInt(5) == 1)
+						{
+							thisFlare.setColor2(thisFlare.getColor2()-0x01000000);
+						}
+
+						//drift down
+						if(rng.nextInt(40) == 1)
+						{
+							thisFlare.setY(thisFlare.getY()+1);
+						}
+					}
+
+
+
+
+				}else
+				{
+					//expiring:   ... working :)
+					fIterator.remove();
+
+				}
+			}
+
+		}//else
+	}
+
+
+	public void move()
+	{
+		// TODO: Implement this method
+
+		//move  old flare/virs
+		if (thisFlare.getTime() < thisFlare.getStage1Time()) 
+		{
+			thisFlare.setY((float) (thisFlare.getY()
+						   + Math.sin(SystemClock.elapsedRealtime()) - 0.2 * thisFlare.getTime() ));
+			//thisFlare.setX((float) thisFlare.getX()
+			//		+ (float) Math.sin(SystemClock.elapsedRealtime()) + (( thisFlare.getTilt() * thisFlare.getTime())/80)  );
+
+			//removing tilt:
+
+
+		} 
+//					else {
+//					}
+//						if (thisFlare.getTime() > thisFlare.getStage1Time()*3) 
+//						{
+//							//
+//						}
+
+		thisFlare.setTime(thisFlare.getTime() + 1);
+		
 	}
 
 	
